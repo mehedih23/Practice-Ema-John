4bbe835a8704e767c4fe55eea0e6de06d7a64087
mehedih23 <nmehedi23@gmail.com>
2022-03-22 16:03:54 +0600

    add here execute other

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,8 +1,11 @@
 import './Products.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
 const Products = (props) => {
-    const { img, name, price, seller, ratings } = props.product;
+    const { addToCart, product } = props;
+    const { img, name, price, seller, ratings } = product;
     return (
         <div className='single-product'>
             <img src={img} alt="product" />
@@ -12,6 +15,10 @@ const Products = (props) => {
                 <p>Manufacturer : {seller}</p>
                 <p><small>ratings : {ratings}</small></p>
             </div>
+            <button onClick={() => addToCart(product)}>
+                <p style={{ marginRight: '8px' }}>Add To Cart</p>
+                <FontAwesomeIcon icon={faCartPlus} />
+            </button>
         </div>
     )
 }
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -10,17 +10,44 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    const [cart, setCart] = useState([]);
+    const [price, setPrice] = useState(0);
+    const [tax, setTax] = useState(0);
+    const [shipping, setShipping] = useState(0);
+    const [grandTotal, setGrandTotal] = useState(0)
+    // Function for single product Button //
+    const addToCart = (product) => {
+        console.log(product);
+        setCart([...cart, product]);
+        const newPrice = product.price + price;
+        setPrice(newPrice);
+        setTax(newPrice * 0.10);
+        setShipping(15);
+    }
+    useEffect(() => {
+        setGrandTotal(price + tax + shipping);
+    }, [price, shipping, tax])
+
     return (
         <div className='shop-container'>
             <div className='product-container'>
                 {
-                    products.map(product => <Products key={product.id} product={product}></Products>)
+                    products.map(product => <Products
+                        key={product.id}
+                        product={product}
+                        addToCart={addToCart}
+                    ></Products>)
                 }
             </div>
             <div className='summary-container'>
-                <p>I am from summary container</p>
+                <p className='order-summary'>Order Summary</p>
+                <p>Selected Item : {cart.length}</p>
+                <p>Price : ${price}</p>
+                <p>TAX : ${tax.toFixed(2)}</p>
+                <p>Shipping Charge : ${shipping}</p>
+                <p><b>Grand Total : ${grandTotal}</b></p>
             </div>
-        </div>
+        </div >
     )
 }
 
