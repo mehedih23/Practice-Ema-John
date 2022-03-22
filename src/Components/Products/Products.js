9c8dd9d05bc321ea71018c4cade4d10f84ad27ff
mehedih23 <nmehedi23@gmail.com>
2022-03-22 14:28:28 +0600

    Product added

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,10 +1,17 @@
 import './Products.css'
 import React from 'react'
 
-const Products = () => {
+const Products = (props) => {
+    const { img, name, price, seller, ratings } = props.product;
     return (
-        <div>
-
+        <div className='single-product'>
+            <img src={img} alt="product" />
+            <div className='product-info'>
+                <h3>{name}</h3>
+                <h2>Price : ${price}</h2>
+                <p>Manufacturer : {seller}</p>
+                <p><small>ratings : {ratings}</small></p>
+            </div>
         </div>
     )
 }
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,10 +1,25 @@
 import './Shop.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Products from '../Products/Products'
 
 const Shop = () => {
-    return (
-        <div>
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    }, [])
 
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+                {
+                    products.map(product => <Products key={product.id} product={product}></Products>)
+                }
+            </div>
+            <div className='summary-container'>
+                <p>I am from summary container</p>
+            </div>
         </div>
     )
 }
